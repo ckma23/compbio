2ce3e0268ed77a168c8d923c58090ed8fc70dd48
ckma23 <curtiskma@gmail.com>
2018-06-24 23:55:24 -0700

    Correcting the automation for ftdock part

--- library/running_on_cluster/ftdock_middleware_cluster.py
+++ library/running_on_cluster/ftdock_middleware_cluster.py
@@ -9,12 +9,12 @@ class FtdockMiddleware(object):
         os.system("rm *.dat")
         os.chdir(os.path.expanduser('~/bioresearch/compbio/files_wip/test_complexes_pdb'))
         testset_pdb_files = os.listdir('.')
-        os.chdir(os.path.expanduser('~/bioresearch/compbio/bin/ftdock-2-dev2/progs-2.0.3'))
         for pdbfile in testset_pdb_files:
             pdbfile = pdbfile.strip("pdb")
             pdbfile = pdbfile.strip(".ent")
             # add a line to clean up the logs each time
             print pdbfile
+            os.chdir(os.path.expanduser('~/bioresearch/compbio/bin/ftdock-2-dev2/progs-2.0.3'))
             os.system("./ftdock -noelec -static ~/bioresearch/compbio/files_wip/rna_seperated_pdbfiles_preprocessperl_testset/%s_*.parsed -mobile ~/bioresearch/compbio/files_wip/protein_seperated_pdbfiles_preprocessperl_testset/%s_*.parsed -out ~/bioresearch/compbio/files_wip/ftdockresults/%sftdock.out > ~/bioresearch/compbio/logs/ftdock_output &" %(pdbfile,pdbfile,pdbfile))
             # sleep FTdock for 5 mins
             os.chdir(os.path.expanduser('~/bioresearch/compbio/files_wip/ftdockresults'))
@@ -29,6 +29,6 @@ class FtdockMiddleware(object):
                 else:
                     print "Not in there yet! Sleeping!"
                     boolean = True
-                    time.sleep(60)            
+                    time.sleep(60)
 
 FtdockMiddleware().ftdock_kicker()
