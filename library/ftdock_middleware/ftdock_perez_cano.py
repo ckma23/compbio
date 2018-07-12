6fed0307cb4642a863603d78a69731da59a4767a
ckma23 <curtiskma@gmail.com>
2018-07-12 01:25:47 -0700

    Implemented build_FtDock

--- library/ftdock_middleware/ftdock_perez_cano.py
+++ library/ftdock_middleware/ftdock_perez_cano.py
@@ -1,18 +1,22 @@
 import os
 import time
 
-from library.energy_formation.energy_formation_calculator import energyCalculator as energyCalculator
+# from library.energy_formation.energy_formation_calculator import energyCalculator as energyCalculator
 
 class FtdockPerezCano(object):
+    # def perez_cano_rankings(self,matrix_file):
     def perez_cano_rankings(self):
         os.system("mkdir ~/bioresearch/compbio/files_wip/perez_cano_rankings")
+        # os.chdir(os.path.expanduser('~/bioresearch/compbio/files_wip/perez_cano_rankings'))
+        # os.system("rm *.perez_cano_scored")
         os.chdir(os.path.expanduser('~/bioresearch/compbio/files_wip/ftdockresults'))
-        pere_cano_matrix = ""
+        pere_cano_matrix = "~/bioresearch/compbio/bin/ftdock-2-dev2/scripts-2.0.3"
         ftdocked_pdb_files = os.listdir('.')
         for ftdockedpdbfile in ftdocked_pdb_files:
             os.chdir(os.path.expanduser('~/bioresearch/compbio/bin/ftdock-2-dev2/progs-2.0.3'))
-            os.system("./rpscore -matrix -in ~/bioresearch/compbio/files_wip/ftdockresults/%s -out ~/bioresearch/compbio/files_wip/perez_cano_rankings/%s.perez_cano_scored > ~/bioresearch/compbio/logs/rpscore &" %(ftdockedpdbfile,ftdockedpdbfile))
-            # os.system("./rpscore -matrix %s -in ~/bioresearch/compbio/files_wip/ftdockresults/%s -out ~/bioresearch/compbio/files_wip/perez_cano_rankings/%s.perez_cano_scored > ~/bioresearch/compbio/logs/rpscore &" %(pere_cano_matrix,ftdockedpdbfile,ftdockedpdbfile))
+            os.system("./rpscore -matrix %s -in ~/bioresearch/compbio/files_wip/ftdockresults/%s -out ~/bioresearch/compbio/files_wip/perez_cano_rankings/%s.perez_cano_scored > ~/bioresearch/compbio/logs/rpscore &" %(pere_cano_matrix,ftdockedpdbfile,ftdockedpdbfile))
+            # os.system("./rpscore -in ~/bioresearch/compbio/files_wip/ftdockresults/%s -out ~/bioresearch/compbio/files_wip/perez_cano_rankings/%s.perez_cano_scored > ~/bioresearch/compbio/logs/rpscore &" %(ftdockedpdbfile,ftdockedpdbfile))
+            time.sleep(60)
 
     def matching_complex_to_rpscore(self):
         native_perez_cano_pose_hash, native_pose_file = energyCalculator.native_checker_pose_hash_retriever(testset_protein)
@@ -34,3 +38,5 @@ class FtdockPerezCano(object):
             #originally looked this
             #native_pose_hash[native_pose_file][line[1]] = {"RMS":,"nativeness":line[3]}
             native_pose_hash[native_pose_file][line[1]]["perez_cano_score"] = line[4]
+
+FtdockPerezCano().perez_cano_rankings()
